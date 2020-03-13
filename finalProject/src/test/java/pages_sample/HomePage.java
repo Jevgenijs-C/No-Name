538a9a6d11b1066f5d113d54b6f3dd5d9d0fe228
Viru1ence <victoriaushkova@gmail.com>
2020-03-13 17:29:55 +0200

    test

--- finalProject/src/test/java/pages_sample/HomePage.java
+++ finalProject/src/test/java/pages_sample/HomePage.java
@@ -1,4 +1,11 @@
 package pages_sample;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import stepDefinitions.Hooks;
+
 
 public class HomePage {
     public String GetHomePage() {
--- finalProject/src/test/java/pages_sample/LogInPage.java
+++ finalProject/src/test/java/pages_sample/LogInPage.java
@@ -1,4 +1,27 @@
 package pages_sample;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LogInPage {
+
+
+    @FindBy (how = How.ID, using = "input-email")
+    private WebElement eMailField;
+    @FindBy (how = How.ID, using = "input-password")
+    private WebElement passwordField;
+    @FindBy (how = How.XPATH, using = "//input[@value='Login']")
+    private WebElement loginButton;
+
+    public void enterMail(){
+        eMailField.sendKeys("testt@test.com");
+    }
+
+    public void enterPassword(){
+        passwordField.sendKeys("12345");
+    }
+    public void logIn(){
+        loginButton.click();
+    }
 }
